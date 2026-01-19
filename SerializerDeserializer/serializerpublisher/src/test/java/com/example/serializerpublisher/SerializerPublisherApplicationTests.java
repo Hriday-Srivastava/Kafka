d0103e8590048d4e164e42a5fbf4a9c767ff1e99
Hriday-Kumar <heartbihar@gmail.com>
2026-01-19 20:10:31 +0530

    Publish the Java Object in Kafka Broker

---
+++ SerializerDeserializer/serializerpublisher/src/test/java/com/example/serializerpublisher/SerializerPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serializerpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializerPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
