4aa872f66e311a4bb6c916325a2706eac477294a
Hriday-Kumar <heartbihar@gmail.com>
2026-01-19 20:12:54 +0530

    Listen the java object from Kafka Broker

---
+++ SerializerDeserializer/deserializerlistener/src/main/java/com/example/deserializerlistener/DeserializerListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.deserializerlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeserializerListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeserializerListenerApplication.class, args);
+	}
+
+}
---
+++ SerializerDeserializer/deserializerlistener/src/test/java/com/example/deserializerlistener/DeserializerListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deserializerlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeserializerListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
